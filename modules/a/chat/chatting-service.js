00e676dcc5936bbbbbcd6cd8df1d3972c05cb1c7
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2020-03-22 13:38:54 +0200

    chatting service

---
+++ modules/a/chat/chatting-service.js
@@ -0,0 +1,41 @@
+function ChattingService() {
+    let me = this;
+
+    let messages = [];
+    let sequence = 0;
+
+
+    this.getMessages = getMessages;
+    this.sendMessage = sendMessage;
+
+
+    function getMessages() {
+        return messages;
+    }
+
+    function sendMessage(content, userName) {
+        if (userName == undefined) {
+            userName = 'Anonymous';
+        }
+
+        let message = new Message(
+            sequence++,
+            new Date(),
+            userName,
+            content
+        );
+        messages.push(message);
+
+        return message;
+    }
+
+
+    function Message(id, createdDate, content, userName) {
+        this.id = id;
+        this.createdDate = createdDate;
+        this.content = content;
+        this.userName = userName;
+    }
+
+
+}
