81aaf5a02da27f9f40fa288dc7e1e21c0ef08411
shangria <shangrialena@gmail,com>
2020-03-28 18:48:54 +0200

    chat minor fixes

--- modules/a/chat/chat.js
+++ modules/a/chat/chat.js
@@ -11,23 +11,24 @@ function Chat() {
 
     function startChat(parentElement) {
         renderChat(parentElement)
-
     }
 
     function renderChat(parentElement) {
         debugger
-        parentElement.innerHTML = `<div>
-            <div class="chat-wrapper"></div>
-                <input type="text" chat-attr="userName">
-                <input type="text" chat-attr="userForm">
-                <button chat-attr="btn">Send</button>
-                <div chat-attr="messagesList"</div>
-         </div>`;
+        parentElement.innerHTML =
+            `<div>
+                <div class="chat-wrapper">
+                    <input type="text" chat-attr="userName">
+                    <input type="text" chat-attr="userForm">
+                    <button chat-attr="btn">Send</button>
+                </div>
+                <div chat-attr="messagesList"></div>
+            </div>`;
 
-        elements.userNameInput = parentElement.querySelector('[chat-attr="userName]');
-        elements.userFormInput = parentElement.querySelector('[chat-attr="userForm]');
+        elements.userNameInput = parentElement.querySelector('[chat-attr="userName"]');
+        elements.userFormInput = parentElement.querySelector('[chat-attr="userForm"]');
         elements.sendBtn = parentElement.querySelector('[chat-attr="btn"]');
-        elements.messagesListInput = parentElement.querySelector('[chat-attr="messagesList"]')
+        elements.messagesListInput = parentElement.querySelector('[chat-attr="messagesList"]');
 
         elements.sendBtn.onclick = function () {
             let message = sendMessage();
@@ -46,14 +47,17 @@ function Chat() {
                 <div>${message.content}</div>
              </div>`;
         elements.messagesListInput.appendChild(messageElement)
-
     }
 
     function sendMessage() {
+        debugger;
+
+        let messageContent = elements.userFormInput.value;
+        let userName = elements.userNameInput.value;
 
         let messageRes = chattingService.sendMessage(
-            elements.messagesListInput.value,
-            elements.userNameInput.value
+            messageContent,
+            userName
         );
         return messageRes
     }
