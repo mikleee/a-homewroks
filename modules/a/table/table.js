8e568e051b93c89b70635cf6198038cda04c086f
shangria <shangrialena@gmail,com>
2020-05-27 00:24:21 +0300

    table

--- modules/a/table/table.js
+++ modules/a/table/table.js
@@ -12,42 +12,62 @@ let persons = [
     {name: 'Tomat', surname: 'Pomidorov'}
 ];
 
+// debugger;
+// let table = document.createElement('table');
+//
+// let thead = document.createElement('thead');
+// thead.innerHTML = `
+//         <tr>
+//             <th>Name</th>
+//             <th>Surname</th>
+//         </tr>`;
+// table.appendChild(thead);
+//
+// let tbody = document.createElement('tbody');
+// let tbodyInnerHtml = `
+//         <tr>
+//             <td>Vasya</td>
+//             <td>Petrov</td>
+//         </tr>
+//         <tr>
+//             <td>Vasya</td>
+//             <td>Petrov</td>
+//         </tr>`;
+// // todo корректно сформировать контент tbody
+// tbody.innerHTML = tbodyInnerHtml;
+// table.appendChild(tbody);
+//
+// let body = document.body;
+// body.appendChild(table);
+
+
 debugger;
 let table = document.createElement('table');
-
 let thead = document.createElement('thead');
-thead.innerHTML = `
-        <tr>
-            <th>Name</th>
-            <th>Surname</th>
-        </tr>`;
+let tr = document.createElement('tr');
+for (let persona of persons) {
+    for (let key in persona) {
+        let th = document.createElement('th');
+        th.innerText = key;
+        tr.appendChild(th);
+    }
+}
+thead.appendChild(tr);
 table.appendChild(thead);
 
 let tbody = document.createElement('tbody');
-let tbodyInnerHtml = `
-        <tr>
-            <td>Vasya</td>
-            <td>Petrov</td>
-        </tr>
-        <tr>
-            <td>Vasya</td>
-            <td>Petrov</td>
-        </tr>`;
-// todo корректно сформировать контент tbody
-tbody.innerHTML = tbodyInnerHtml;
+let trBody = document.createElement('tr');
+let personaValue;
+for (let persona of persons) {
+    for (let key in persona) {
+        let tdBody = document.createElement('td');
+        personaValue = persona[key];
+        tdBody.innerText = personaValue;
+        trBody.appendChild(tdBody);
+    }
+}
+tbody.appendChild(trBody);
 table.appendChild(tbody);
-
 let body = document.body;
 body.appendChild(table);
 
-
-// let table = `<table style="border: 2px blueviolet solid">`;
-// table += `<thead><tr>`;
-// for (let persona of persons) {
-//     for (let key in persona)
-//         table += `<td>${key}</td>`;
-// }
-// table += `</tr></thead>`;
-//
-// table += `<tbody><tr>`;
-//
