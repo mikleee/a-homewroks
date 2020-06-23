1d91d9e11588568f7ccadc09a8f95d7fc759be7c
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2020-06-23 10:31:56 +0300

    departments -> department form

--- modules/m/departments/departments.js
+++ modules/m/departments/departments.js
@@ -14,27 +14,51 @@
         table.setAttribute('id', 'departments');
 
         let thead = document.createElement('thead');
-        thead.innerHTML = `
-        <tr>
-            <th>Id</th>    
-            <th>Created</th>    
-            <th>Name</th>    
-            <th>Description</th>    
-            <th></th>    
-        </tr>`;
+        {
+            let tr = document.createElement('tr');
+
+            let thId = document.createElement('th');
+            thId.innerHTML = 'Id';
+            tr.appendChild(thId);
+
+            let thCreated = document.createElement('th');
+            thCreated.innerHTML = 'Created';
+            tr.appendChild(thCreated);
+
+            let thName = document.createElement('th');
+            thId.innerHTML = 'Name';
+            tr.appendChild(thName);
+
+            let thDescription = document.createElement('th');
+            thId.innerHTML = 'Description';
+            tr.appendChild(thDescription);
+
+            let thActions = document.createElement('th');
+            let addDepartmentButton = document.createElement('button');
+            addDepartmentButton.innerText = 'Add';
+            addDepartmentButton.onclick = function () {
+                table.style.display = 'none';
+
+                departmentsService.deleteDepartment(department);
+                tr.remove();
+            };
+            tr.appendChild(thActions);
+
+        }
         table.appendChild(thead);
 
 
         let tbody = document.createElement('tbody');
         for (let department of departments) {
             let tr = document.createElement('tr');
+            tbody.appendChild(tr);
 
             let tdId = document.createElement('td');
             tdId.innerHTML = department.id;
             tr.appendChild(tdId);
 
             let tdCreated = document.createElement('td');
-            tdCreated.innerHTML = tarnslateDate(department.created);
+            tdCreated.innerHTML = translateDate(department.created);
             tr.appendChild(tdCreated);
 
             let tdName = document.createElement('td');
@@ -56,15 +80,75 @@
             tdActions.appendChild(deleteDepartmentButton);
 
             tr.appendChild(tdActions);
-
-            tbody.appendChild(tr);
         }
         table.appendChild(tbody);
 
         return table;
     }
 
-    function tarnslateDate(date) {
+    function createDepartmentForm(department) {
+        let container = document.createElement('div');
+        container.setAttribute('id', 'departmentForm');
+
+        if (department.id) {
+            // id
+            let idGroup = document.createElement('div');
+            container.appendChild(idGroup);
+
+            let idLabel = document.createElement('label');
+            idLabel.innerHTML = 'Id';
+            idGroup.appendChild(idLabel);
+
+            let idSpan = document.createElement('span');
+            idSpan.innerHTML = department.id;
+            idGroup.appendChild(idSpan);
+
+            // created
+            let createdGroup = document.createElement('div');
+            container.appendChild(createdGroup);
+
+            let createdLabel = document.createElement('label');
+            createdLabel.innerHTML = 'Created';
+            createdGroup.appendChild(createdLabel);
+
+            let createdSpan = document.createElement('inputSpan');
+            createdSpan.innerHTML = translateDate(department.created);
+            idGroup.appendChild(createdSpan);
+        }
+
+        // name
+        let nameGroup = document.createElement('div');
+        container.appendChild(nameGroup);
+
+        let nameLabel = document.createElement('label');
+        nameLabel.innerHTML = 'Name';
+        nameLabel.setAttribute('for', 'departmentName');
+        nameGroup.appendChild(nameLabel);
+
+        let nameInput = document.createElement('input');
+        nameInput.value = department.name;
+        nameLabel.setAttribute('id', 'departmentName');
+        nameGroup.appendChild(nameInput);
+
+
+        // description
+        let descriptionGroup = document.createElement('div');
+        container.appendChild(descriptionGroup);
+
+        let descriptionLabel = document.createElement('label');
+        descriptionLabel.innerHTML = 'Description';
+        descriptionLabel.setAttribute('for', 'departmentDescription');
+        descriptionGroup.appendChild(descriptionLabel);
+
+        let descriptionInput = document.createElement('input');
+        descriptionInput.value = department.description;
+        descriptionLabel.setAttribute('id', 'departmentDescription');
+        descriptionGroup.appendChild(descriptionInput);
+
+        return container;
+    }
+
+    function translateDate(date) {
         return date.toLocaleString();
     }
 
